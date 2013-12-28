4c2822e85d5342e0526c6979329912be80862d29
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2013-12-27 21:25:14 -0500

    added index page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Saved Searches on Craigslist</title>
+	<style type="text/css">
+		body { font-family: Consolas, Courier, monospace; font-size: 12px; margin: 0; padding: 20px; }
+		ol { margin: 0; padding: 0; }
+		ol li { display: none; background-color: #000; color: white; margin-bottom: 1px; padding: 8px; list-style-position: inside; }
+		ol li a { color: white; font-weight: bold; }
+	</style>
+</head>
+<body>
+
+	<ol id="emailLog"></ol>
+
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+
+</body>
+</html>
