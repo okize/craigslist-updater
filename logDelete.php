1de9d9c243ad4b6a00d55916b5b9e758119ed8b9
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2013-12-27 21:12:19 -0500

    added logDelete page

---
+++ logDelete.php
@@ -0,0 +1,17 @@
+<?php
+
+// turn off error reporting
+error_reporting(0);
+
+// json file
+$file = 'log.json';
+
+// if file doesn't exist, mention that, otherwise delete it
+if (!file_exists($file)) {
+	echo 'No log file found';
+} else {
+	unlink($file);
+	echo 'Log file deleted';
+}
+
+?>
